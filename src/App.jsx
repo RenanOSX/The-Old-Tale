f10b7980aa905e858c6a8c7d099f0121137e9c97
Darkromano <77361353+RenanOSX@users.noreply.github.com>
2024-08-16 02:57:14 -0300

    Animação de texto principal, nova logo, logo ao lado do nome do jogo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Header from "./components/Header";
 import monster from "./assets/monster.png";
+import icon from "./assets/magic_wand.png";
 
 function App() {
   return (
     <div>
-      <Header title="Projeto Integrador" description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos impedit placeat magni blanditiis maiores recusandae itaque molestias porro nemo, nobis fugiat officiis praesentium alias eius unde, earum reprehenderit nihil deserunt?"/>
+      <Header title="Praecantatio Idle" image={icon} description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos impedit placeat magni blanditiis maiores recusandae itaque molestias porro nemo, nobis fugiat officiis praesentium alias eius unde, earum reprehenderit nihil deserunt?"/>
       <main>
         <section id="box">
-          <h1 class="center">oihrfriuhg</h1>
+          <h1 class="center">Monster Name</h1>
           <img src={monster} class="center"></img>
         </section>
       </main>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,7 @@ import logo from "../assets/logo.png";
 export default function Header({title, description, image=logo}) {
     return(
         <header>
-            <img src={image} alt={description} />
-            <h1>{title}</h1>
+            <h1>{title}<img src={image} alt={description}/></h1>
             <p>
             {description}
             </p>
