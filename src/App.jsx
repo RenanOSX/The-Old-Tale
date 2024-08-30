1e3985d732791a9c934401d3ae38e5c0c2bff662
samuel cesca <samuel1cesca@gmail.com>
2024-08-30 03:04:34 -0300

    removing-home

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/homeScreen/home";
 import FirstScreen from "./pages/firstScreen/first_screen";
 
 function App() {
   return (
    <Router>
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/" element={<FirstScreen/>}/>
       <Route path="/firstScreen" element={<FirstScreen/>}/>
     </Routes>
    </Router>
