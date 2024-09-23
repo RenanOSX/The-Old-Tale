ee0f0cafdc180937cf0bd12ce51c5689381845d6
samuel cesca <samuel1cesca@gmail.com>
2024-09-23 11:50:53 -0300

    introd

--- src/pages/IntroductionScreen/IntroductionScreen.jsx
+++ src/pages/IntroductionScreen/IntroductionScreen.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
+
+import arrowIcon from '../../assets/icons/arrow_circle_right.png';
+
 import './IntroductionScreen.css'; // Certifique-se de que o caminho está correto
+
+import { textSalvamento } from '../../constants/Texts';
 
 const IntroductionScreen = ({ introduction, onProceed }) => {
   return (
     <div className="introduction-screen">
-      <div className="introduction-content">
-        <h1>Bem-vindo ao Jogo</h1>
-        <p>{introduction}</p>
-        <button onClick={onProceed}>Começar</button>
+       <div className='line1'>
+        <div className="introduction-content">
+          <h1>Bem-vindo ao Jogo</h1>
+          <p>{introduction}</p>
+        </div>
+
+        <div className="introduction-content">
+          <h1>Salvamento</h1>
+          <p>{textSalvamento}</p>
+          <div className="loading-spinner">
+            <div className="spinner"></div>
+          </div>
+        </div>
+      </div>
+     
+      <div className='line2'>
+        <div className="introduction-content">
+          <h1>Progresso do Jogador</h1>
+          <p>{introduction}</p>
+        </div>
+      </div>
+
+      <div className="proceed-icon" onClick={onProceed}>
+        <img src={arrowIcon} alt="Proceed" />
       </div>
     </div>
   );
